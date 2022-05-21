570135b08937bbd7d1bf369e14bb5b1446a0a992
Richard van Hees <r.m.van.hees@sron.nl>
2022-05-21 18:18:33 +0200

    layout_draw_image.py: this test modules works now nearly perfect for aspect in (1, 2, 3, 4)

--- examples/layout_draw_image.py
+++ examples/layout_draw_image.py
@@ -71,6 +71,7 @@ def add_img_fig_box(axx_c, aspect: int, fig_info: FIGinfo) -> None:
                    horizontalalignment='left',
                    bbox={'facecolor': 'white', 'pad': 4})
 
+# -------------------------
 def draw_figure(aspect: int, side_panels='one') -> None:
     """
     Show figure with given aspect ratio
@@ -81,10 +82,10 @@ def draw_figure(aspect: int, side_panels='one') -> None:
              2: {'figsize': (13, 6.25),
                  'w_ratios': (1. , 10. , 0.5, 1.5),
                  'h_ratios': (5., 1.)},                  # 10 x 5
-             3: {'figsize': (15, 5.5),
+             3: {'figsize': (15, 5.375),
                  'w_ratios': (1. , 12. , 0.5, 1.5),
                  'h_ratios': (4., 1.)},                  # 12 x 4
-             4: {'figsize': (17, 4.75),
+             4: {'figsize': (17, 5.125),
                  'w_ratios': (1. , 14. , 0.5, 1.5),
                  'h_ratios': (3.5, 1.)}}.get(aspect)     # 14 x 3.5
     
@@ -120,11 +121,14 @@ def draw_figure(aspect: int, side_panels='one') -> None:
     #        Defines the relative heights of the rows. Each row gets a
     #        relative height of height_ratios[i] / sum(height_ratios).
     #        If not given, all rows will have the same height.
-    gspec = fig.add_gridspec(2, 4, wspace=0.05 / aspect, hspace=0.03,
+    gspec = fig.add_gridspec(2, 4,
+                             wspace=0.1 / max(2, aspect-1), hspace=0.05,
                              width_ratios=attrs['w_ratios'],
                              height_ratios=attrs['h_ratios'],
-                             left=.135, right=.9, bottom=.115,
-                             top=.865 - .05 * (aspect - 1) )
+                             left=.135 + .005 * (aspect-1),
+                             right=.9 - .005 * (aspect-1),
+                             bottom=.115 + .01 * (aspect-1),
+                             top=.865 - .025 * (aspect-1) )
     # else:
     #    gspec = fig.add_gridspec(2, 4, wspace=0.05 / aspect, hspace=0.025,
     #                             width_ratios=(1., 4. * aspect, .3, .7),
@@ -160,9 +164,6 @@ def draw_figure(aspect: int, side_panels='one') -> None:
     _ = plt.colorbar(pcm, cax=axx_c, label='value')
 
     fig_info = FIGinfo()
-    # use 5 to test x-small font (above)
-    # use 7 to test xx-small font (above)
-    # use 9 to test x-small font (below)
     for ii in range(5):
         fig_info.add(f'text line {ii+1}', 'blah blah blah')
     add_img_fig_box(axx_c, aspect, fig_info)
