85b4b8c7d671a5d690dcaba42fb8ad0176d1a99a
Richard van Hees <r.m.van.hees@sron.nl>
2023-09-12 13:44:02 +0200

    Improved module description

--- src/moniplot/__init__.py
+++ src/moniplot/__init__.py
@@ -17,7 +17,9 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
+#
+"""This is the SRON Python package `moniplot`.
+
 Moniplot is a Python data visualization library for (satellite)
 instrument monitoring.
 """
